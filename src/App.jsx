a0578cf34586f50b599d25a664e4954665945eb2
shohidul2015 <shd_islam@hotmail.com>
2025-12-09 21:28:37 +0100

    Add: Added Hero and Features sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Pricing from "./components/Pricing"
+import Testimonials from "./components/Testimonials"
 
 
 function App() {
 
   return (
-    <div className="font-bold">
-      App page!
-
+     <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Footer />
     </div>
   )
 }
